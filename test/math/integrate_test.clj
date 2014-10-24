b1ed5226088411bcb931b99231100075e93bd448
Colin Smith <colin.smith@gmail.com>
2014-10-24 09:04:24 -0700

    a few more tests

--- test/math/integrate_test.clj
+++ test/math/integrate_test.clj
@@ -8,10 +8,20 @@
 
 (def ^:private near (within 1e-6))
 (def ^:private natural-log (partial integrate / 1.))
+(def ^:private sine (partial integrate #(Math/cos %) 0.))
 
 (deftest integrals
   (testing "easy"
     (is (near 0.333333 (integrate #(* % %) 0. 1.)))
     (is (near 1.0 (natural-log (Math/exp 1.))))
-    (is (near 2.302585 (natural-log 10.))))
-    )
+    (is (near 0 (sine Math/PI)))
+    (is (near 1 (sine (/ Math/PI 2))))
+    ))
+
+;; (deftest with-timing
+;;   (testing "easy"
+;;     (prn (time (integrate #(* % %) 0. 1.)))
+;;     (prn (time (natural-log (Math/exp 1.))))
+;;     (prn (time (sine Math/PI)))
+;;     (prn (time (sine (/ Math/PI 2))))
+;;     ))
