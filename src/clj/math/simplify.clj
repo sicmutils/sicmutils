ed026b08716579fbdf01a2c5fb993f58c0d16e2d
Colin Smith <colin.smith@gmail.com>
2014-12-31 15:08:46 -0800

    simplification now functional.
    Got rid of the transient collections by threading the expression-seen map through the code. It's uglier but arguably more in the modern style (unless we were going to use a monad or something, which probably isn't that bad an idea).

--- src/clj/math/simplify.clj
+++ src/clj/math/simplify.clj
@@ -12,58 +12,70 @@
   [symbol-generator expr-> ->expr known-operations]
   (let [base-simplify #(expr-> % ->expr)]
     (fn [expr]
-      ; "if a tree falls in the forest" functional code
-      (let [xmap (transient {})]
-        (letfn [(simplify-expression
-                  [expr]
-                  (-> expr analyze base-simplify (backsubstitute (persistent! xmap))))
-                (analyze
-                  [expr]
-                  (if (and (sequential? expr)
-                           (not (= (first expr) 'quote)))
-                    (let [analyzed-expr (map analyze expr)]
-                      ;; at this point all subexpressions are canonical
-                      (if (and (known-operations (sym/operator analyzed-expr))
-                               true #_"this is where the exponent integrality test would go")
-                        analyzed-expr
-                        (or (xmap analyzed-expr) (new-kernels analyzed-expr))))
-                    expr))
-                (new-kernels
-                  [expr]
-                  (let [simplified-expr (map base-simplify expr)]
-                    (if-let [v (sym/symbolic-operator (sym/operator simplified-expr))]
-                      (let [w (apply v (sym/operands simplified-expr))]
-                        (if (and (sequential? w)
-                                 (= (sym/operator w) (sym/operator simplified-expr)))
-                          (add-symbols! w)
-                          (analyze w)))
-                      (add-symbols! simplified-expr))
-                    ))
-                (add-symbols!
-                  [expr]
-                  (let [new (map add-symbol! expr)]
-                    (add-symbol! new)))
-                (add-symbol!
-                  [expr]
-                  (if (and (sequential? expr)
-                           (doall expr)
-                           (not (= (first expr) 'quote)))
-                    (or (xmap expr)
-                        (let [newvar (symbol-generator)]
-                          (conj! xmap [expr newvar])
-                          newvar))
-                    expr))
-                (backsubstitute
-                  [expr xmap]
-                  (let [mapx (into {} (for [[k v] xmap] [v k]))
-                        bsub (fn bsub [v]
-                               (cond (sequential? v) (doall (map bsub v))
-                                     (symbol? v) (let [w (mapx v)]
-                                                   (if w (bsub w) v))
-                                     :else v))]
-                    (bsub expr)))]
-          ;; TODO: note that we only return the vector for debugging & testing.
-          ;; once backsubstitute is implemented, we won't need this and can
-          ;; perhaps get rid of the call to (doall) in (add-symbol!).
-          (simplify-expression expr))))))
+      (letfn [(simplify-expression
+                [expr]
+                (let [[expr-map analyzed-expr] (analyze {} expr)]
+                  (->> analyzed-expr base-simplify (backsubstitute expr-map))))
+              (analyze
+                [expr-map expr]
+                (if (and (sequential? expr)
+                         (not (= (first expr) 'quote)))
+                  (let [[expr-map analyzed-expr] (map-with-state expr-map analyze expr)]
+                    ;; at this point all subexpressions are canonical
+                    (if (and (known-operations (sym/operator analyzed-expr))
+                             true #_"this is where the exponent integrality test would go")
+                      [expr-map analyzed-expr]
+                      (if-let [existing-expr (expr-map analyzed-expr)]
+                        [expr-map existing-expr]
+                        (new-kernels expr-map analyzed-expr))))
+                  [expr-map expr]))
+              (new-kernels
+                [expr-map expr]
+                (let [simplified-expr (map base-simplify expr)]
+                  (if-let [v (sym/symbolic-operator (sym/operator simplified-expr))]
+                    (let [w (apply v (sym/operands simplified-expr))]
+                      (if (and (sequential? w)
+                               (= (sym/operator w) (sym/operator simplified-expr)))
+                        (add-symbols expr-map w)
+                        (analyze expr-map w)))
+                    (add-symbols expr-map simplified-expr))
+                  ))
+              (add-symbols
+                [expr-map expr]
+                (let [[new-expr-map new-expr] (map-with-state expr-map add-symbol expr)]
+                  (add-symbol new-expr-map new-expr)))
+              (add-symbol
+                [expr-map expr]
+                (if (and (sequential? expr)
+                         (doall expr)
+                         (not (= (first expr) 'quote)))
+                  (if-let [existing-expr (expr-map expr)]
+                    [expr-map existing-expr]
+                    (let [newvar (symbol-generator)]
+                      [(conj expr-map [expr newvar]) newvar]))
+                  [expr-map expr]))
+              (backsubstitute
+                [expr-map expr]
+                (let [mapx (into {} (for [[k v] expr-map] [v k]))
+                      bsub (fn bsub [v]
+                             (cond (sequential? v) (doall (map bsub v))
+                                   (symbol? v) (let [w (mapx v)]
+                                                 (if w (bsub w) v))
+                                   :else v))]
+                  (bsub expr)))
+              (map-with-state
+                ;; Maps f over coll while maintaining state. The function
+                ;; f is called with [state, v] for each value v in col, and
+                ;; is expected to return a pair containing the new state and
+                ;; (f v). The result is a pair with the final state and
+                ;; the sequence of the values of (f v).
+                [initial-state f coll]
+                (reduce
+                  (fn [[state acc] val]
+                    (let [[new-state f-val] (f state val)]
+                      [new-state (conj acc f-val)]))
+                  [initial-state []]
+                  coll))
+              ]
+        (simplify-expression expr)))))
 
