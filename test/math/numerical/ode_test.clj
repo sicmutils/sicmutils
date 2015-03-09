1863d8fead9a6eb64df42de7e5ce38e60795cc0e
Colin Smith <colin.smith@gmail.com>
2015-03-09 14:46:15 -0700

    comments

--- test/math/numerical/ode_test.clj
+++ test/math/numerical/ode_test.clj
@@ -9,11 +9,11 @@
     (let [result ((state-advancer (constantly identity)) (up 1.) 1. 1.e-10)]
       (is ((v/within 1e-8) (Math/exp 1) (nth result 0))))
     (let [states (atom [])
-          result ((evolve (constantly identity))
-                  (up 1.)
-                  #(swap! states conj [%1 %2])
-                  0.1
-                  1
-                  1e-10)]
+          result ((evolve (constantly identity)) ;; solve: y' = y
+                  (up 1.)                        ;;        y(0) = 1
+                  #(swap! states conj [%1 %2])   ;; accumulate intermediate results
+                  0.1                            ;; ... with a mesh size of 0.1
+                  1                              ;; solve until t = 1
+                  1e-10)]                        ;; accuracy desired
       (is (= (count @states) 11))
       (is ((v/within 1e-8) (Math/exp 1) (first result))))))
