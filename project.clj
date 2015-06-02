93c29b782324f81ebb2167ddfb0199a20c49900d
Colin Smith <colin.smith@gmail.com>
2015-06-02 11:28:40 -0700

    detach logging from library

--- project.clj
+++ project.clj
@@ -4,18 +4,13 @@
   :license {:name "GPLv3"
             :url "http://www.opensource.org/licenses/GPL-3.0"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/data.json "0.2.6"]
-                 [org.apache.commons/commons-math3 "3.4.1"]
-                 [org.clojure/math.numeric-tower "0.0.4"]
                  [org.clojure/tools.logging "0.3.1"]
                  [com.google.guava/guava "18.0"]
-                 [log4j/log4j "1.2.17" :exclusions [javax.mail/mail
-                                                    javax.jms/jms
-                                                    com.sun.jdmk/jmxtools
-                                                    com.sun.jmx/jmxri]]]
+                 [org.apache.commons/commons-math3 "3.4.1"]
+                 [org.clojure/math.numeric-tower "0.0.4"]]
   :source-paths ["src/clj"]
   :java-source-paths ["src/java"]
-  :jvm-opts ["-server"]
+  :jvm-opts ["-server" "-Djava.util.logging.config.file=logging.properties"]
   :repl-options {:prompt (fn [ns] (str "[" ns "] > "))
                  :welcome "clojure algebra system"
                  :init-ns math.repl}
