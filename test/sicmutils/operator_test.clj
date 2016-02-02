e1649904ef52b1cbf8690d3089fd0ccd50a0332f
Colin Smith <colin.smith@gmail.com>
2016-02-01 21:37:38 -0800

    enable new operator unit tests
    from @alexgian

--- test/sicmutils/operator_test.clj
+++ test/sicmutils/operator_test.clj
@@ -28,8 +28,8 @@
 (def ff (literal-function 'ff [0 0] 0))
 (def gg (literal-function 'gg [0 0] 0))
 
-            
-;; Test operations with Operators            
+
+;; Test operations with Operators
 (deftest Operator-tests
   (testing "that our known Operators work with basic arithmetic"
     (is (every? operator? [(+ D 1)(+ 2 D)(- D 3)(- 4 D)(* 5 D)(* D 6)]))
@@ -37,33 +37,27 @@
     )
   (testing "that they compose with other Operators"
     (is (every? operator? [(* D D)(* D (partial 0))(*(partial 0) D)(* (partial 0)(partial 1))])))
-  (comment testing "that their arithmetic operations compose correctly, as per SICM -  'Our Notation'"
-      (is (= (((* (+ D 1)(- D 1)) f) 'x) 
-             (+ (((expt D 2) f) 'x) (* -1 (f 'x))) )))
-  (comment testing "that Operators compose correctly with functions"
-      (is (= ((D ((* (- D g)(+ D 1)) f)) 'x)
-	     (+ (* -1 (((expt D 2) f) 'x) (g 'x))
-	        (* -1 (g 'x) ((D f) 'x))
-	        (* -1 ((D f) 'x) ((D g) 'x))
-	        (* -1 ((D g) 'x) (f 'x))
-	        (((expt D 2) f) 'x)
-	        (((expt D 3) f) 'x)))))
-  (comment testing "that basic arithmetic operations work on multivariate literal functions"
-      (is (= (((+  D  D) ff) 'x 'y)
-             (down (* 2 (((partial 0) ff) 'x 'y)) (* 2 (((partial 1) ff) 'x 'y)))))
-      (is (= (((-  D  D) ff) 'x 'y)
-             (down 0 0)))
-      (is (= (((*  D  D) ff) 'x 'y)
-             (down
-               (down (((partial 0) ((partial 0) ff)) 'x 'y) (((partial 0) ((partial 1) ff)) 'x 'y))
-               (down (((partial 1) ((partial 0) ff)) 'x 'y) (((partial 1) ((partial 1) ff)) 'x 'y)))))
-      (is (= (((*  (partial 1)  (partial 0)) ff) 'x 'y)
-             (((partial 1) ((partial 0) ff)) 'x 'y)))))
-             
-    ;;; more testing to come as we implement multivariate literal functions that rely on operations on structures....             
+  (testing "that their arithmetic operations compose correctly, as per SICM -  'Our Notation'"
+    (is (= (simplify (((* (+ D 1)(- D 1)) f) 'x))
+           '(+ (((expt D 2) f) x) (* -1 (f x))) )))
+  (testing "that Operators compose correctly with functions"
+    (is (= (simplify ((D ((* (- D g)(+ D 1)) f)) 'x))
+           '(+ (* -1 (((expt D 2) f) x) (g x))
+               (* -1 (g x) ((D f) x))
+               (* -1 ((D f) x) ((D g) x))
+               (* -1 ((D g) x) (f x))
+               (((expt D 2) f) x)
+               (((expt D 3) f) x)))))
+  (testing "that basic arithmetic operations work on multivariate literal functions"
+    (is (= (simplify (((+  D  D) ff) 'x 'y))
+           '(down (* 2 (((∂ 0) ff) x y)) (* 2 (((∂ 1) ff) x y)))))
+    (is (= (simplify (((-  D  D) ff) 'x 'y))
+           '(down 0 0)))
+    (is (= (((*  D  D) ff) 'x 'y)
+           (down
+            (down (((partial 0) ((partial 0) ff)) 'x 'y) (((partial 0) ((partial 1) ff)) 'x 'y))
+            (down (((partial 1) ((partial 0) ff)) 'x 'y) (((partial 1) ((partial 1) ff)) 'x 'y)))))
+    (is (= (((*  (partial 1)  (partial 0)) ff) 'x 'y)
+           (((partial 1) ((partial 0) ff)) 'x 'y)))))
 
-
-
-
-      
-
+    ;;; more testing to come as we implement multivariate literal functions that rely on operations on structures....
