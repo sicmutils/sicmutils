a61273d6228f225b9cde63402144040548a11c21
Colin Smith <colin.smith@gmail.com>
2016-09-05 18:57:47 -0700

    wip

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject net.littleredcomputer/sicmutils "0.9.5"
+(defproject net.littleredcomputer/sicmutils "0.9.6-SNAPSHOT"
   :description "A port of the Scmutils computer algebra/mechanics system to Clojure"
   :url "http://github.com/littleredcomputer/sicmutils"
   :license {:name "GPLv3"
@@ -9,7 +9,6 @@
                  [com.google.guava/guava "19.0"]
                  [org.apache.commons/commons-math3 "3.6.1"]
                  [org.clojure/math.numeric-tower "0.0.4"]
-                 [org.clojure/test.check "0.9.0"]
                  [org.clojure/tools.nrepl "0.2.12"]]
   :main sicmutils.repl
   :jvm-opts ["-Djava.util.logging.config.file=logging.properties"]
@@ -20,4 +19,5 @@
   :target-path "target/%s"
   :test-selectors {:default (complement :long)}
   :profiles {:uberjar {:aot :all}
-             :travis {:jvm-opts ["-Xmx512M"]}})
+             :travis {:jvm-opts ["-Xmx512M"]}
+             :test {:dependencies [[org.clojure/test.check "0.9.0"]]}})
