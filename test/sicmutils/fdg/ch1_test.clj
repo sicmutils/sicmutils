868f740545a5275c8f3cc7eed1201277ca3ce7a7
Colin Smith <colin.smith@gmail.com>
2017-06-11 18:23:15 -0700

    fix copyright

--- test/sicmutils/fdg/ch1_test.clj
+++ test/sicmutils/fdg/ch1_test.clj
@@ -1,6 +1,7 @@
 ;
-; Copyright (C) 2017 Colin Smith.
-; This work is based on the Scmutils system of MIT/GNU Scheme.
+; Copyright © 2017 Colin Smith.
+; This work is based on the Scmutils system of MIT/GNU Scheme:
+; Copyright © 2002 Massachusetts Institute of Technology
 ;
 ; This is free software;  you can redistribute it and/or modify
 ; it under the terms of the GNU General Public License as published by
