86b591a781ba622a619e156e3ba7a5f92cf2668f
Colin Smith <colin.smith@gmail.com>
2018-09-17 18:11:14 -0700

    wip

--- src/sicmutils/numerical/integrate.clj
+++ src/sicmutils/numerical/integrate.clj
@@ -78,6 +78,7 @@
     value))
 
 (defn carlson-rf [x y z]
+  "From W.H. Press, Numerial Recipes in C++, 2ed. NR::rf from section 6.11"
   (let [errtol 0.0025
         tiny 1.5e-38
         big 3.0e37
@@ -118,6 +119,8 @@
                (Math/sqrt ave))))))))
 
 (defn elliptic-f [phi k]
+  "Legendre elliptic integral of the first kind F(φ, k).
+   See W.H. Press, Numerical Recipes in C++, 2ed. eq. 6.11.19"
   (let [s (Math/sin phi)]
     (* s (carlson-rf (Math/pow (Math/cos phi) 2)
                      (* (- 1 (* s k))
