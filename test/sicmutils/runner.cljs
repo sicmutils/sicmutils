0e04c649ae05940dcfe19b69504f91ac990c4a03
Sam Ritchie <samritchie@google.com>
2020-08-15 23:16:44 -0600

    add cljs test back in

--- test/sicmutils/runner.cljs
+++ test/sicmutils/runner.cljs
@@ -29,6 +29,7 @@
 (doo-tests 'pattern.match-test
            'pattern.rule-test
            'sicmutils.calculus.derivative-test
+           'sicmutils.numerical.compile-test
            'sicmutils.analyzer-test
            'sicmutils.complex-test
            'sicmutils.euclid-test
