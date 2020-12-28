c8fe40063a2ede2dd043d410ae3288682e15fe7b
Teodor Heggelund <teodor+git@iterate.no>
2020-12-28 12:00:36 +0100

    How does matrix write?

--- src/sicmutils/units/scm_api.clj
+++ src/sicmutils/units/scm_api.clj
@@ -1,7 +1,9 @@
 (ns sicmutils.units.scm-api
   "Copying the original scmutils API here, so that we can evaluate it. Defer
   finding an idiomatic Clojure API. Consider not pushing this namespace at all."
-  (:require [sicmutils.units.units :as u]
+  (:require [sicmutils.value :as v]
+            [sicmutils.generic :as g]
+            [sicmutils.units.units :as u]
             [sicmutils.units.with-units :as wu]))
 
 (def &meter    (u/units u/SI [1 0 0 0 0 0 0] 1))
@@ -20,5 +22,34 @@
      (.scale meter-squared)])
   ;; => [[2 0 0 0 0 0 0] 1]
 
+
+  ;;
+  )
+
+(comment
   (u/*units &meter &meter)
+  ;; => #object[sicmutils.units.units.Units 0x6d3d2d71 "sicmutils.units.units.Units@6d3d2d71"]
+
+  ;; This is completely unreadable. I'd like something a little better for
+  ;; debugging. Do I ... implement a toString on Units?
+
+  ;; Look at matrix, again.
+  ;; matrix has a tostring calling str: (toString [_] (str v))
+
+  (.toString (u/*units &meter &meter))
+  ;; => "sicmutils.units.units.Units@22737a40"
+
+  (str (u/*units &meter &meter))
+  ;; => "sicmutils.units.units.Units@3ece403"
+
+  ;; These two already does the same thing.
+  ;; What does matrix do?
+  (g/* [1 2 3] [10 20 30])
+  ;; => #object[sicmutils.structure.Structure 0x6f544435 "(up (up 10 20 30) (up 20 40 60) (up 30 60 90))"]
+
+  (g/* [[1 2] [3 4]] [[10 20] [30 40]])
+  (up (up (up (up 10 20) (up 30 40)) (up (up 20 40) (up 60 80)))
+      (up (up (up 30 60) (up 90 120)) (up (up 40 80) (up 120 160))))
+  ;; this doesn't look like a matrix.
+
   )
