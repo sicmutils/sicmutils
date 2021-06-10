bfa3a4a434d7fcaf0eb44f47773627b33c9fea66
Sameer Padala <sameer@padalas.net>
2021-06-10 09:02:51 -0700

    Fix ;; comments

--- demo/src/demo/minimal.cljs
+++ demo/src/demo/minimal.cljs
@@ -1,29 +1,29 @@
 (ns demo.minimal
   (:require
-    ; sicmutils.env has aliases for common functions. If you are using a REPL, run
-    ; `sicmutils.env/bootstrap-repl!` to pull in all the aliases into the current namespace.
+    ;; sicmutils.env has aliases for common functions. If you are using a REPL, run
+    ;; `sicmutils.env/bootstrap-repl!` to pull in all the aliases into the current namespace.
     [sicmutils.env :as s :include-macros true]))
 
 (defn output [expr]
-  ; `simplify` collapses expressions while `->infix` prints them using Unicode operators. 
+  ;; `simplify` collapses expressions while `->infix` prints them using Unicode operators. 
   (let [string (s/->infix (s/simplify expr))]
-    ; Depending on your build options, the default output might go to the browser console or the
-    ; terminal's console.
+    ;; Depending on your build options, the default output might go to the browser console or the
+    ;; terminal's console.
     (println string)
-    ; Also hackishly append to the <body> for browser builds.
+    ;; Also hackishly append to the <body> for browser builds.
     (if (resolve 'js/document) (.append js/document.body string "\n"))))
 
 (output "Running demo.minimal...\n")
 
-; It's important to use SICM's math operators so that the result remains a ratio.
+;; It's important to use SICM's math operators so that the result remains a ratio.
 (output (s/- (s/* 7 (s// 1 2)) 2))    ; 3/2
-; The default math operators will turn this expression into a float.
+;; The default math operators will turn this expression into a float.
 (output (- (* 7 (/ 1 2)) 2))          ; 1.5
 
-; Define a basic expression.
+;; Define a basic expression.
 (def f (s/* 3 (s/expt s/sin 2)))
 (output (f 'x))                       ; 3 sin²(x)
-; And take the derivative.
+;; And take the derivative.
 (output ((s/D f) 'x))                 ; 6 sin(x) cos(x)
 
-; More examples are available at /demo.clj
+;; More examples are available at /demo.clj
