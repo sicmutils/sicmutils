a4226efb5e439d1002e5daf412b0e22bc9b19f3d
Sam Ritchie <sritchie09@gmail.com>
2022-06-04 08:33:59 -0600

    final

--- build.clj
+++ build.clj
@@ -1,5 +1,5 @@
 (ns build
-  "Build for the sicmutils library."
+  "tools.build declarations for the sicmutils library."
   (:require [clojure.tools.build.api :as b]))
 
 ;; ## Variables
@@ -14,17 +14,12 @@
      (format "%s-%s" version suffix)
      version)))
 
-(def snapshot (->version "SNAPSHOT"))
-
 ;; source for jar creation.
 (def class-dir "target/classes")
+(def basis (b/create-basis {:project "deps.edn"}))
 
 (defn ->jar-file [version]
   (format "target/%s-%s.jar" (name lib) version))
-
-(def basis (b/create-basis {:project "deps.edn"}))
-
-(def built-jar-version-file "target/built-jar-version.txt")
 
 ;; ## Tasks
 
@@ -34,23 +29,17 @@
   opts)
 
 (defn jar
-  "Build library jar file.
+  "Builds a jar containing all library code and
 
-  Also writes built version to target/built-jar-version.txt for easy peasy
-  pickup by any interested downstream operation.
-
-  We use the optional :version-suffix to optionally distinguish local installs
-  from production releases.
-
-  For example, when installing for a cljdoc preview suffix is: cljdoc-preview."
+  Optionally supply a string via `:version-suffix` to append `-<suffix>` to the
+  generated version."
   [{:keys [version-suffix] :as opts}]
   (let [version  (->version version-suffix)
         jar-file (->jar-file version)]
     (b/write-pom {:class-dir class-dir
                   :lib lib
                   :version version
-                  :donkey "punch"
-                  :scm {:tag version}
+                  :scm {:tag (str "v" version)}
                   :basis basis
                   :src-pom "template/pom.xml"
                   :src-dirs ["src"]
@@ -61,13 +50,15 @@
                  :target-dir class-dir})
     (b/jar {:class-dir class-dir
             :jar-file jar-file})
-    (spit built-jar-version-file version)
+
     (println (str "Created " jar-file "."))
     (assoc opts
            :jar-file jar-file
            :built-jar-version version)))
 
-(defn install [opts]
+(defn install
+  "Clean, generate a jar and install the jar into the local Maven repository."
+  [opts]
   (clean opts)
   (let [{:keys [built-jar-version jar-file]} (jar opts)]
     (b/install {:class-dir class-dir
@@ -75,10 +66,13 @@
                 :version built-jar-version
                 :basis basis
                 :jar-file jar-file})
-    (println (str "Installed " jar-file " to local maven repository."))
+    (println (str "Installed " jar-file " to local Maven repository."))
     opts))
 
-(defn publish [opts]
+(defn publish
+  "Generates a jar with all project sources and resources and publishes it to
+  Clojars."
+  [opts]
   (clean opts)
   (let [{:keys [jar-file]} (jar opts)]
     (println (str "Publishing " jar-file " to Clojars!"))
@@ -91,91 +85,3 @@
     ;; TODO put a catch here if the version already exists?
     (println "Published.")
     opts))
-
-
-;; (ns build
-;;   "sicmutils build script. TODO finish conversion!
-
-;;   clojure -T:build ci
-
-;;   clojure -T:build run-doc-tests :aliases '[:cljs]'
-
-;;   Run tests:
-;;   clojure -X:test
-;;   clojure -X:test:master
-
-;;   For more information, run:
-
-;;   clojure -A:deps -T:build help/doc"
-;;   (:refer-clojure :exclude [test])
-;;   (:require [clojure.tools.build.api :as b]
-;;             [org.corfield.build :as bb]))
-
-;; (def lib 'sicmutils/sicmutils)
-;; (defn- the-version [patch] (format "0.22.%s" patch))
-;; (def version (the-version (b/git-count-revs nil)))
-;; (def snapshot (the-version "999-SNAPSHOT"))
-
-;; (defn eastwood [opts]
-;;   (bb/run-task opts [:eastwood]))
-
-;; (defn gen-doc-tests
-;;   "Generate tests from doc code blocks."
-;;   [opts]
-;;   (bb/run-task opts [:gen-doc-tests]))
-
-;; (defn run-doc-tests
-;;   "Generate and run doc tests.
-
-;;   Optionally specify :aliases vector:
-;;   [:1.9] -- test against Clojure 1.9 (the default)
-;;   [:1.10] -- test against Clojure 1.10.3
-;;   [:1.11] -- test against Clojure 1.11.0
-;;   [:master] -- test against Clojure 1.12 master snapshot
-;;   [:cljs] -- test against ClojureScript"
-;;   [{:keys [aliases] :as opts}]
-;;   (gen-doc-tests opts)
-;;   (bb/run-tests
-;;    (assoc opts :aliases
-;;           (-> [:test-doc]
-;;               (into aliases)
-;;               (into (if (some #{:cljs} aliases)
-;;                       [:test-doc-cljs]
-;;                       [:test-doc-clj])))))
-;;   opts)
-
-;; (defn test
-;;   "Run basic tests."
-;;   [opts]
-;;   (-> opts
-;;       (update :aliases (fnil conj []) :1.11)
-;;       (bb/run-tests)))
-
-;; (defn ci
-;;   "Run the CI pipeline of tests (and build the JAR).
-
-;;   Default Clojure version is 1.9.0 (:1.9) so :elide
-;;   tests for #409 on that version."
-;;   [opts]
-;;   (-> opts
-;;       (bb/clean)
-;;       (assoc :lib lib :version (if (:snapshot opts) snapshot version))
-;;       (as-> opts
-;;           (reduce (fn [opts alias]
-;;                     (run-doc-tests (assoc opts :aliases [alias])))
-;;                   opts
-;;                   [:cljs :elide :1.10 :1.11 :master]))
-;;       (eastwood)
-;;       (as-> opts
-;;           (reduce (fn [opts alias]
-;;                     (bb/run-tests (assoc opts :aliases [alias])))
-;;                   opts
-;;                   [:cljs :elide :1.10 :1.11 :master]))
-;;       (bb/clean)
-;;       (assoc :src-pom "template/pom.xml")
-;;       (bb/jar)))
-
-;; (defn deploy "Deploy the JAR to Clojars." [opts]
-;;   (-> opts
-;;       (assoc :lib lib :version (if (:snapshot opts) snapshot version))
-;;       (bb/deploy)))
